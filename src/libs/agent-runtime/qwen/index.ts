e515cb847b6f35fb5fe6d8b8ac29ee520784fd7d
sxjeru <sxjeru@gmail.com>
2024-06-15 19:44:50 +0800

    fix

--- src/libs/agent-runtime/qwen/index.ts
+++ src/libs/agent-runtime/qwen/index.ts
@@ -8,12 +8,28 @@ export const LobeQwenAI = LobeOpenAICompatibleFactory({
   chatCompletion: {
     handlePayload: (payload) => {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { temperature, ...restPayload } = payload;
+      const { temperature, messages, ...restPayload } = payload;
       const top_p = payload.top_p;
       const model = payload.model;
+
+      if (Array.isArray(messages)) {
+        messages = messages.map(message => {
+          if (!Array.isArray(message.content)) {
+            return {
+              ...message,
+              content: {
+                text: message.content,
+                type: 'text'
+              }
+            };
+          }
+          return message;
+        });
+      }
       
       return model.includes('-vl-') ? {
         ...restPayload,
+        messages,
         stream: restPayload.stream ?? true,
         top_p: top_p && top_p >= 1 ? 0.9999 : top_p,
       } as OpenAI.ChatCompletionCreateParamsStreaming : {
