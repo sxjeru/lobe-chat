497dd0ce8dd1808876c444bc4cca219bfdad77da
sxjeru <sxjeru@gmail.com>
2024-07-19 09:10:20 +0800

    Update route.ts

--- src/app/api/chat/google/route.ts
+++ src/app/api/chat/google/route.ts
@@ -18,8 +18,8 @@ export const runtime = 'edge';
 // regions include Dublin (dub1, Ireland), Paris (cdg1, France), Frankfurt (fra1, Germany), London (lhr1, UK), and Stockholm (arn1, Sweden).
 // refs: https://ai.google.dev/gemini-api/docs/available-regions
 export const preferredRegion = [
-  'icn1',
-  'sin1',
+  // 'icn1',
+  // 'sin1',
   'hnd1',
   'kix1',
   'bom1',
