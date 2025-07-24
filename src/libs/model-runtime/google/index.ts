057318a795d9b2e907106295f268972d891633d4
sxjeru <sxjeru@gmail.com>
2025-07-24 10:39:37 +0800

    Update index.ts

--- src/libs/model-runtime/google/index.ts
+++ src/libs/model-runtime/google/index.ts
@@ -518,7 +518,7 @@ export class LobeGoogleAI implements LobeRuntimeAI {
 
     // 统一的错误类型判断函数
     const getErrorType = (code: number | null, message: string): ILobeAgentRuntimeErrorType => {
-      if (code === 401 || message.includes('API_KEY_INVALID')) {
+      if (code === 400 && message.includes('API key not valid')) {
         return AgentRuntimeErrorType.InvalidProviderAPIKey;
       } else if (code === 429) {
         return AgentRuntimeErrorType.QuotaLimitReached;
@@ -638,11 +638,7 @@ export class LobeGoogleAI implements LobeRuntimeAI {
       try {
         const jsonString = message.slice(startIndex);
         const json: GoogleChatErrors = JSON.parse(jsonString);
-        const bizError = json[0];
 
-        if (bizError?.reason === 'API_KEY_INVALID') {
-          return { ...defaultError, errorType: AgentRuntimeErrorType.InvalidProviderAPIKey };
-        }
         return { error: json, errorType: AgentRuntimeErrorType.ProviderBizError };
       } catch {
         // 忽略解析错误
