cc0ec62836b185aa7faec65355c19fe3ed14a8d9
sxjeru <20513115+sxjeru@users.noreply.github.com>
2026-01-01 13:22:05 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,21 @@
 import { defineConfig } from './src/libs/next/config/define-config';
 
-const nextConfig = defineConfig({});
+const nextConfig = defineConfig({
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  experimental: {
+    webpackBuildWorker: true,
+    webpackMemoryOptimizations: true,
+  },
+
+  webpack: (config, { dev }) => {
+    if (!dev) {
+        config.cache = false;
+    }
+    return config;
+  },
+});
 
 export default nextConfig;
